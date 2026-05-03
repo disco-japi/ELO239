aa0c1f3644ff759f0fb41530190bccfdfe9a6f64
Alealrusm <alealr@usm.cl>
2026-05-02 22:57:21 -0400

    Delete Tarea2/Etapa_2 directory

--- Tarea2/Etapa_2/Cellular.java
+++
@@ -1,5 +0,0 @@
-public class Cellular extends Equipo {
-    public Cellular(String owner, double x, double y, double r, double theta, double dt) {
-        super(owner, x, y, r, theta, dt);
-    }
-}
--- Tarea2/Etapa_2/Tablet.java
+++
@@ -1,6 +0,0 @@
-public class Tablet extends Equipo {
-    public Tablet(String owner, double x, double y, double r, double theta, double dt) {
-        super(owner, x, y, r, theta, dt);
-    }
-}
-
